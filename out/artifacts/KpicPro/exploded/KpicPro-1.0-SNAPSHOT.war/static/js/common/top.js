5844264448c33fffb2c9145889f5b25b0d4f83d1
JuwinS1993 <1807301715@qq.com>
2016-04-28 20:32:08 +0800

    私信系统，话题首页云图集

--- out/artifacts/KpicPro/exploded/KpicPro-1.0-SNAPSHOT.war/static/js/common/top.js
+++ out/artifacts/KpicPro/exploded/KpicPro-1.0-SNAPSHOT.war/static/js/common/top.js
@@ -1,5 +1,9 @@
 $().ready(function(){
     $.post("/user/all/notread/news", function(data){
-        $("#user_news").append(data);
+        if(data != "" && data != null){
+            if(data != 0){
+                $("#user_news").append("<span class=\"badge\" style=\"background-color: #FF79BC\">"+data+"</span>");
+            }
+        }
     });
 });
